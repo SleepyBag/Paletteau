21ba9c07cb3f9ef22dea125e6afb210d3f400e26
Qianming Xue <xueqianming200@gmail.com>
2022-06-26 00:09:00 +0800

    Add: process info plugin

---
+++ Plugins/ProcessInfo/Main.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Paletteau.Plugin;
+using Paletteau.Infrastructure;
+
+namespace Palette
+{
+    class Main : IPlugin
+    {
+        public List<Result> Query(Query query)
+        {
+            var process = query.BackgroundProcess;
+            var results = new List<Result>
+            {
+                new Result
+                {
+                    Title = process.ProcessName,
+                    SubTitle = "process name"
+                },
+                new Result
+                {
+                    Title = process.MainWindowTitle,
+                    SubTitle = "window title"
+                },
+                new Result {
+                    Title = process.Id.ToString(),
+                    SubTitle = "process id"
+                },
+                new Result {
+                    Title = process.TotalProcessorTime.ToString(),
+                    SubTitle = "total processor time"
+                },
+                new Result
+                {
+                    Title = process.UserProcessorTime.ToString(),
+                    SubTitle = "user processor time"
+                },
+                new Result
+                {
+                    Title = process.VirtualMemorySize64.ToString(),
+                    SubTitle = "virtual memory size"
+                },
+                new Result
+                {
+                    Title = process.BasePriority.ToString(),
+                    SubTitle = "priority"
+                },
+            };
+            return results;
+        }
+
+        public void Init(PluginInitContext context)
+        {
+        }
+
+    }
+}
