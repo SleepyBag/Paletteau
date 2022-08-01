639480733417d78901c8771389e220df7624ecb3
Qianming Xue <xueqianming200@outlook.com>
2022-08-01 19:39:11 +0800

    Add: maximize / minimize for window management

--- Paletteau.Infrastructure/Windows/WindowHandler.cs
+++ Paletteau.Infrastructure/Windows/WindowHandler.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.Diagnostics.Eventing.Reader;
 
 namespace Paletteau.Infrastructure.Windows
 {
     public class WindowHandler
     {
         #region consts
+        // used for SetWinodwPos
         private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
         private static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
 
@@ -31,6 +33,21 @@ namespace Paletteau.Infrastructure.Windows
 
         private const UInt32 NO_RESIZE = SWP_NOMOVE | SWP_NOSIZE;
 
+        // used for ShowWindow
+        private const int SW_HIDE = 0;                 // Hides the window and activates another window.
+        private const int SW_NORMAL = 1;               // Activates and displays a window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when displaying the window for the first time.
+        private const int SW_SHOWMINIMIZED = 2;        // Activates the window and displays it as a minimized window.
+        private const int SW_SHOWMAXIMIZED = 3;        // Activates the window and displays it as a maximized window.
+        private const int SW_SHOWNOACTIVATE = 4;       // Displays a window in its most recent size and position. This value is similar to SW_SHOWNORMAL, except that the window is not activated.
+        private const int SW_SHOW = 5;                 // Activates the window and displays it in its current size and position.
+        private const int SW_MINIMIZE = 6;             // Minimizes the specified window and activates the next top-level window in the Z order.
+        private const int SW_SHOWMINNOACTIVE = 7;      // Displays the window as a minimized window. This value is similar to SW_SHOWMINIMIZED, except the window is not activated.
+        private const int SW_SHOWNA = 8;               // Displays the window in its current size and position. This value is similar to SW_SHOW, except that the window is not activated.
+        private const int SW_RESTORE = 9;              // Activates and displays the window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when restoring a minimized window.
+        private const int SW_SHOWDEFAULT = 10;         // Sets the show state based on the SW_ value specified in the STARTUPINFO structure passed to the CreateProcess function by the program that started the application.
+        private const int SW_FORCEMINIMIZE = 11;       // Minimizes a window, even if the thread that owns the window is not responding. This flag should only be used when minimizing windows from a different thread.
+
+        // used for GetWindowLong
         const int GWL_EXSTYLE = (-20);
 
         const UInt32 WS_EX_TOPMOST = 0x0008;
@@ -40,17 +57,33 @@ namespace Paletteau.Infrastructure.Windows
         [DllImport("user32.dll", SetLastError = true)] 
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         static extern int GetWindowTextLength(IntPtr hWnd);
+
         [DllImport("user32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool IsWindow(IntPtr hWnd);
+
         [DllImport("user32.dll", SetLastError = true)]
         private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
         [DllImport("user32.dll", SetLastError = true)]
         private static extern IntPtr SetActiveWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsIconic(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool IsZoomed(IntPtr hWnd);
         #endregion
 
         public IntPtr hWnd;
@@ -102,6 +135,31 @@ namespace Paletteau.Infrastructure.Windows
             var title = new StringBuilder(length);
             GetWindowText(this.hWnd, title, length);
             return title.ToString();
+        }
+        
+        public bool Minimize()
+        {
+            return ShowWindow(this.hWnd, SW_MINIMIZE);
+        }
+
+        public bool Restore()
+        {
+            return ShowWindow(this.hWnd, SW_RESTORE);
+        }
+
+        public bool Maximize()
+        {
+            return ShowWindow(this.hWnd, SW_SHOWMAXIMIZED);
+        }
+
+        public bool IsMaximized()
+        {
+            return IsZoomed(this.hWnd);
+        }
+
+        public bool IsMinimized()
+        {
+            return IsIconic(this.hWnd);
         }
     }
 }
--- Plugins/WindowManagement/Main.cs
+++ Plugins/WindowManagement/Main.cs
@@ -16,6 +16,7 @@ namespace WindowManagement
     public class Main : IPlugin
     {
         List<WindowHandler> HiddenWindows = new List<WindowHandler>();
+        List<WindowHandler> MinimizedWindows = new List<WindowHandler>();
 
         public void Init(PluginInitContext context)
         {
@@ -30,7 +31,9 @@ namespace WindowManagement
                 new Result() { Title = "close main window", Action = _ => { process.CloseMainWindow(); return true; } },
                 new Result() { Title = "force kill process", Action = _ => { process.Kill(); return true; } },
                 new Result() { Title = "hide window", Action = _ => { window.Hide(); HiddenWindows.Add(window); return true; } },
+                new Result() { Title = "minimize window", Action = _ => { window.Minimize(); MinimizedWindows.Add(window); return true; } },
             };
+            // pin on top
             if (window.IsOnTop())
             {
                 results.Add(new Result()
@@ -47,6 +50,22 @@ namespace WindowManagement
                     Action = _ => { window.PinOnTop(); window.SetActive(); return true; }
                 });
             }
+            // maximize
+            if (window.IsMaximized())
+            {
+                results.Add(new Result() {
+                    Title = "restore maximized window", 
+                    Action = _ => { window.Restore(); return true; } 
+                });
+            }
+            else
+            {
+                results.Add(new Result() {
+                    Title = "maximize window", 
+                    Action = _ => { window.Maximize(); return true; } 
+                });
+            }
+
             // remove all closed hidden windows
             HiddenWindows = HiddenWindows.Where(hiddenWindow => hiddenWindow.Exists()).ToList();
             // put hidden windows into results for recovery
@@ -64,8 +83,33 @@ namespace WindowManagement
                     }
                 );
             }
+
+            // remove all closed or restored minimized windows
+            MinimizedWindows = MinimizedWindows.Where(
+                minimizedWindow => minimizedWindow.Exists() && minimizedWindow.IsMinimized()).ToList();
+            // put minimized windows into results for recovery
+            foreach (var minimizedWindow in MinimizedWindows)
+            {
+                results.Add(
+                    new Result() { 
+                        Title = "restore minimized window: " + minimizedWindow.GetWindowTitle(), 
+                        Action = _ => { 
+                            minimizedWindow.Restore(); 
+                            minimizedWindow.SetActive(); 
+                            MinimizedWindows.Remove(minimizedWindow); 
+                            return true; 
+                        }
+                    }
+                );
+            }
+
             // filter by query
             results = StringMatcher.FilterListResultByTitle(results, query.Search);
+            // keep original order - there aren't too many results
+            foreach (var tuple in results.Select((result, index) => new {result, index}))
+            {
+                tuple.result.Score = results.Count() - tuple.index;
+            }
             return results;
         }
     }
